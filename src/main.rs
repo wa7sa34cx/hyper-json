617ae450f3e832294dd59834915184b8e6e14d66
Alex Black <wa7@sa34.cx>
2021-12-06 15:00:19 +0300

    Move all logic to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+//! Main module
+//!
+//! Pattern is often predictable, and anything predictable can be hacked.
+
+use hyper_json::run;
+
+#[tokio::main]
+async fn main() {
+    run().await.unwrap();
 }
