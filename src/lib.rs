2dc829946f2f29dea89d8a8e60b8fdb5b7ac3e7f
Alex Black <wa7@sa34.cx>
2021-12-06 15:41:19 +0300

    First test

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,27 @@
 //! Library module
 
-use hyper::Client;
+mod logging;
+
+use dotenv::dotenv;
+use hyper::{Client, Uri};
 
 pub async fn run() -> anyhow::Result<()> {
-    // let client = Client::new();
+    // Create client
+    let client = Client::new();
 
-    println!("Hello world!");
+    // Get URI from .env and parse it
+    let uri: Uri = get_env("ENDPOINT").parse()?;
+
+    // Await the response...
+    let resp = client.get(uri).await?;
+
+    // Print HTTP status
+    log::info!("Response: {}", resp.status());
 
     Ok(())
+}
+
+fn get_env(env: &'static str) -> String {
+    dotenv().ok();
+    std::env::var(env).unwrap_or_else(|_| panic!("Cannot get the {} env variable", env))
 }
